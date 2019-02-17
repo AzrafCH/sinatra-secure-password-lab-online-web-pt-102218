829a83885184ab82d2ae120edd90d6070c2bc597
Azraf Chowdhury <achowdhur@ltu.edu>
2019-02-17 22:21:17 +0000

    Automatically backed up by Learn

--- db/migrate/20190217221947_create_users.rb
+++ db/migrate/20190217221947_create_users.rb
@@ -4,10 +4,10 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :password_digest
-    end 
-  end 
+    end
+  end
 
-  def down 
+  def down
     drop_table :users
   end
 end
